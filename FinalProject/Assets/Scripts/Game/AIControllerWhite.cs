ea150d7e5ac603d04a61e5178a37f69ed45d4667
NathanLapensee16 <230375127+NathanLapensee16@users.noreply.github.com>
2026-04-19 17:46:52 -0400

    Added Rotation Input Info to WhiteTankAIController

--- FinalProject/Assets/Scripts/Game/AIControllerWhite.cs
+++ FinalProject/Assets/Scripts/Game/AIControllerWhite.cs
@@ -1,5 +1,6 @@
 using Unity.Behavior;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIControllerWhite : AIController
 {
@@ -18,5 +19,28 @@ public class AIControllerWhite : AIController
     private void Update()
     {
         steeringContext.Detect(DetectorType.Obstacle | DetectorType.Barrel | DetectorType.Tank | DetectorType.Detonator);
+
+        Vector3 Direction = steeringContext.Solve(SteeringBehaviourType.ObstacleAvoidance | SteeringBehaviourType.Wandering);
+
+        Vector2 a = new Vector2(Direction.x, Direction.z);
+        Vector2 b = new Vector2(transform.forward.x, transform.forward.z);
+        float degreesA = Mathf.Atan2(a.y, a.x) + Mathf.Rad2Deg;
+        float degreesB = Mathf.Atan2(b.y, b.x) + Mathf.Rad2Deg;
+        float difference = degreesB - degreesA;
+
+        difference = difference % 360.0f;
+        if (difference < 0.0f) difference += 360.0f;
+        if (difference < 180.0f) difference -= 360.0f;
+
+        float sign = Mathf.Sign(difference);
+        float alignment = Vector2.Dot(a,b);
+        if (alignment < 0.99f)
+        {
+            tank.TankRotation = sign;           
+        }
+        else
+        {
+            tank.TankRotation = 0.0f;
+        }
     }
 }
